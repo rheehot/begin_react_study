7c0b812855aab6d5d2dadd252e19310e3ab7b9ff
peter L <peter.l@uvaper.com>
2022-02-23 14:18:56 +0900

    배열 추가/ 제거 / 수정

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
-import React, { useState, useRef } from "react";
+import React, { useRef, useState } from "react";
 import UserList from "./UserList";
 import CreateUser from "./CreateUser";
 
 function App() {
-  const style = {
-    margin: "20px",
-  };
-
   const [inputs, setInputs] = useState({
     username: "",
     email: "",
   });
-
-  const { username, email } = inputs; //input 요소의 비구조화 할당
-
+  const { username, email } = inputs;
   const onChange = (e) => {
     const { name, value } = e.target;
     setInputs({
@@ -21,28 +15,48 @@ function App() {
       [name]: value,
     });
   };
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: "velopert",
+      email: "public.velopert@gmail.com",
+      active: true,
+    },
+    {
+      id: 2,
+      username: "tester",
+      email: "tester@example.com",
+      active: false,
+    },
+    {
+      id: 3,
+      username: "liz",
+      email: "liz@example.com",
+      active: false,
+    },
+  ]);
 
-  const nextId = useRef(4); //생성될 요소의 다음 id값을 정의
-
+  const nextId = useRef(4);
   const onCreate = () => {
     const user = {
       id: nextId.current,
       username,
       email,
     };
-    setUsers([...users, user]); // ...users로 새로운 배열을 복사한 이유는 저번에도 설명했다 시피 불변성을 유지해야 react가 렌더링을 진행한다.
+    setUsers(users.concat(user));
+
     setInputs({
       username: "",
       email: "",
     });
-    console.log(nextId.current);
     nextId.current += 1;
   };
 
   const onRemove = (id) => {
+    // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
+    // = user.id 가 id 인 것을 제거함
     setUsers(users.filter((user) => user.id !== id));
   };
-
   const onToggle = (id) => {
     setUsers(
       users.map((user) =>
@@ -50,37 +64,16 @@ function App() {
       )
     );
   };
-  const [users, setUsers] = useState([
-    {
-      id: 1,
-      username: "carrot",
-      email: "carrot@gamil.com",
-      active: true,
-    },
-    {
-      id: 2,
-      username: "apple",
-      email: "apple@gamil.com",
-      active: true,
-    },
-    {
-      id: 3,
-      username: "tomato",
-      email: "tomato@gamil.com",
-      active: false,
-    },
-  ]);
-
   return (
-    <div style={style}>
+    <>
       <CreateUser
         username={username}
         email={email}
         onChange={onChange}
         onCreate={onCreate}
       />
-      <UserList users={users} onRemove={onRemove} />
-    </div>
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+    </>
   );
 }
 
--- src/UserList.js
+++ src/UserList.js
@@ -1,32 +1,37 @@
 import React from "react";
 
 function User({ user, onRemove, onToggle }) {
-  const { username, email, id, active } = user;
   return (
     <div>
       <b
         style={{
-          color: active ? "green" : "black",
           cursor: "pointer",
+          color: user.active ? "green" : "black",
         }}
-        onClick={() => onToggle(id)}
+        onClick={() => onToggle(user.id)}
       >
-        {username}
+        {user.username}
       </b>
       &nbsp;
-      <span>({email})</span>
-      <button onClick={() => onRemove(id)}>삭제</button>
+      <span>({user.email})</span>
+      <button onClick={() => onRemove(user.id)}>삭제</button>
     </div>
   );
 }
 
-function UserList({ users, onRemove }) {
+function UserList({ users, onRemove, onToggle }) {
   return (
-    <>
+    <div>
       {users.map((user) => (
-        <User user={user} key={user.id} onRemove={onRemove} />
+        <User
+          user={user}
+          key={user.id}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
       ))}
-    </>
+    </div>
   );
 }
+
 export default UserList;
